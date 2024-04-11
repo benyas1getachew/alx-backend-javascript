ca152490f94e00fbcfc28f5255b0416bdab73274
benyas1getachew <99409114+benyas1getachew@users.noreply.github.com>
2024-04-12 01:27:40 +0530

    my commit

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,12 @@
 export default function guardrail(mathFunction) {
-  const array = []; // create an empty array
+  const array = []; 
 
-  try { // try to run the function and push the result to the array
+  try {
     array.push(mathFunction());
-  } catch (err) { // if the function throws an error, push the error to the array
+  } catch (err) {
     array.push(err.toString());
-  } finally { // always push the finally result to the array
+  } finally {
     array.push('Guardrail was processed');
   }
-  return array; // return the array
+  return array;
 }
